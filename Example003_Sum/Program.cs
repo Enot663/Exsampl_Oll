ada961eef9403f442dba6f102200b46a4efa889e
Andrey <andrejsatykov@yandex.ru>
2022-11-26 00:02:12 +0300

    Example 003 Sum s dopolniyhoi

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,3 +1,6 @@
-﻿int numberA = 5;
-int numberB = 6;
+﻿Console.Clear();
+int numberA = new Random().Next(1,10);
+int numberB = new Random().Next(1,10);
+Console.WriteLine("Число А = " + numberA);
+Console.WriteLine("Число В = " + numberB);
 Console.WriteLine(numberB + numberA);
