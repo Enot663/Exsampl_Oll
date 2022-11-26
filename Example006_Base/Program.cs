b6c661075e23d5b2d79576828a3083b06bb0da78
Andrey <andrejsatykov@yandex.ru>
2022-11-26 11:33:17 +0300

    Example 006 Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+int a = 1;
+int b = 4;
+int c = 6;
+int d = 9;
+int e = 5;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.WriteLine("Мксимальный вес = " + max);
