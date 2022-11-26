a7d64a3cb0c108e6a1068226f401c51dd83dfcc2
Andrey <andrejsatykov@yandex.ru>
2022-11-26 14:10:06 +0300

    Example 007 Megic

---
+++ Example007_Megic/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+//Console.SetCursorPosition(10, 4);
+//Console.WriteLine("+");
+
+int xa = 80,  ya = 1,
+    xb = 0,  yb = 2,
+    xc = 0, yc = 20;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
