dfe9d87c0c0e070cc196a13a451a50e31e1fc551
Andrey <andrejsatykov@yandex.ru>
2022-11-26 11:08:31 +0300

    Создан новый файл

---
+++ Example005_ifels/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите Ваше имя:  ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же МАША!!!!!!!");
+}
+else
+{
+    Console.WriteLine("Привет " + username + "!");
+}
